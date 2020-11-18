4f07d105fee8124de209378f76c59dcd26df3200
Davidoodle <57453111+Dmelkumo@users.noreply.github.com>
2020-11-18 00:16:26 -0500

    pi camera

--- rover/app.py
+++ rover/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 import RPi.GPIO as GPIO
 import time
+from camera_pi import Camera #Miguel Grinberg's pi camera implementation
 
 app = Flask(__name__)
 
@@ -96,6 +97,19 @@ def getDist():
     
     return tooClose
 
+#Miguel Grinberg's pi camera implementation (slightly adapted for our use)
+#initializes the camera class
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+#gets the image element to display it in
+@app.route('/livestream')
+def livestream():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route("/", methods=["GET"])
 def home():
